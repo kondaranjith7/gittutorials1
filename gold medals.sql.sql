57bace85fe30519ea5ce035a1595f43caca476b3
kondaranjith7 <kondaranjith7@gmail.com>
2023-09-30 14:14:07 +0530

    this is the first commit

---
+++ gold medals.sql.sql
@@ -0,0 +1,15 @@
+select * from events
+---------sub query
+select gold as player_name,count(1) as no_medals
+from events
+where gold not in (select silver from events union all select bronze from events)
+group by gold
+----------group by    having cte
+with cte as(
+select gold as player_name,'gold' as medal_type from events
+union all select silver,'silver' as medal_type from events
+union all select bronze,'bronze' as medal_type from events)
+select player_name,count(1) as no_medals
+from cte
+group by player_name
+having count(distinct medal_type) =1and max(medal_type)= 'gold'
